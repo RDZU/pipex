fcc69f066f83ec089d27c48c09bf4a18ef59c466
Randolph David (razamora) <davidzamorau@gmail.com>
2024-07-25 22:32:50 +0200

    85%

--- pipex_main.c
+++ pipex_main.c
@@ -6,7 +6,7 @@
 /*   By: razamora <razamora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 13:09:55 by razamora          #+#    #+#             */
-/*   Updated: 2024/07/14 19:05:45 by razamora         ###   ########.fr       */
+/*   Updated: 2024/07/25 16:03:22 by razamora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ char	**ft_find_path(char **envp)
 {
 	char **var_env;
 	char *value_path;
-	
 	while (*envp != NULL)
 	{
 		value_path = *envp++;
@@ -33,14 +32,8 @@ char	**ft_find_path(char **envp)
 	}
 	var_env = ft_split(value_path,':');	
 	return(var_env);
+	
 }
-
-void	ft_error(char *str, int code)
-{
-	ft_putendl_fd(str, 2);
-	exit(code);
-}
-
 void	ft_free_memory(char **tab)
 {
 	size_t	i;
@@ -54,6 +47,12 @@ void	ft_free_memory(char **tab)
 	free(tab);
 }
 
+void	ft_error(char *str, int code)
+{
+	ft_putendl_fd(str, 2);
+	exit(code);
+}
+
 void exec_command(char *cmd, char *cmd_route, char *envp, char **full_cmd)
 {
 	//printf("exec command  %s %s \n", cmd_route, cmd);
@@ -61,13 +60,12 @@ void exec_command(char *cmd, char *cmd_route, char *envp, char **full_cmd)
 	int	flag;
 
 	flag = 0;
-	if (execve(cmd_route, full_cmd, NULL) < 0)
+	if (execve(cmd_route, full_cmd,NULL) < 0)
 	{
-		ft_putstr_fd("command not found: ", 2);
-		ft_putendl_fd(cmd_route, 2);
-		ft_free_memory(full_cmd);
-		exit(0);
+		printf("command error");
+		exit(1);
 	}
+	
 //	printf("%d", flag);
 //	exit(0);
 }
@@ -78,34 +76,24 @@ void ft_check_command(char *cmd, char **envp)
 	char	**full_cmd;
 	char	**path;
 
-	if (access(cmd, F_OK || X_OK) == 0)
-	{
-		printf("se puede ejecutar  %s \n", cmd);
-	}
-	else
-	{
-		printf("no se puede ejecutar  %s \n", cmd);
-	}
-
-	//exit(1);
-	path = ft_find_path(envp);
-	if (path == NULL)
-		ft_error("Error: PATH not found", 2);
 	i = 0;
 	full_cmd = ft_split(cmd, ' ');
 	cmd = ft_strjoin("/", full_cmd[0]);
-	// exec_command(cmd, path[i], *envp,full_cmd);
-	
+	//exec_command(cmd, path[i], *envp,full_cmd);
+	//printf("exec command  %s %s \n", cmd, path[i]);
+	path = ft_find_path(envp);
+	if (path == NULL)
+		ft_error("Error: PATH not found", 2);
 	while (path[i] != NULL)
 	{
 		path[i] = ft_strjoin(path[i], cmd);
 		if (access(path[i], F_OK | X_OK) == 0)
-			//exec_command(cmd, path[i], *envp,full_cmd);
-			printf("se puede ejecutar  %s \n", path[i]);
+			exec_command(cmd, path[i], *envp,full_cmd);
 		i++;
 	}
 	ft_putstr_fd("command not found: ", 2);
 	ft_putstr_fd(cmd, 2);
+	exit(1);
 }
 // child
 // fd = open(O_CREAT | O_RDWR | O_TRUNC, 0644)
@@ -114,45 +102,67 @@ void	process_one(char **argv, char **envp, int *file_pipe)
 	int	fd;
 
 	fd = open(argv[1], 0);
-	dup2(fd, 0);
+	dup2(fd,0);
 	dup2(file_pipe[1],1);
 	close(file_pipe[0]);
 	ft_check_command(argv[2], envp);
 	close(fd);
 }
 
-void	process_two(char **argv, char **envp, int *file_pipe)
+void	process_two(char **argv, char **envp, int *file_pipe, int argc)
 {
 	char **path;
 	int	fd;
-	fd = open(argv[4],O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (fd < 0)
-		exit(1);
+	fd = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0777);
 	dup2(fd,1);
 	dup2(file_pipe[0],0);
 	close(file_pipe[1]);
-	ft_check_command(argv[3], envp);
+	ft_check_command(argv[argc - 2], envp);
 	close(fd);
 }
 
-
-
 int	main(int argc, char **argv, char **envp)
 {
+	// char *cmd_args[] = {"ls","-l", NULL};
+	// execve("/usr/bin/ls", cmd_args, NULL);
+	// exit(1);
 	int	file_pipe[2];
-	int	pid;
+	pid_t	id;
 
-	if(argc != 5)
-		ft_error("Error: invalid number of arguments", 2);
-	if(pipe(file_pipe) == -1)
-		exit(-1);
-	pid = fork();
-	if(pid < 0)
-		exit(-1);
-	if(pid == 0)
-		process_one(argv, envp,file_pipe);
+	
+	if (argc < 5)
+		(perror("minimum 5 argc"), exit(1));
+	if (pipe(file_pipe) == -1)
+		exit(0);
+	id = fork();
+	if (id < 0)
+		return(1);
+	if (id == 0)
+		process_one(argv, envp, file_pipe);
 	else
-		process_two(argv, envp, file_pipe);
+		process_two(argv, envp, file_pipe,argc);
 	waitpid(-1, NULL,0);
 	return (0);
 }
+
+
+/*
+static int	do_exec(char **command, char **env)
+{
+	char	*cmd;
+
+	if (access(command[0], F_OK | X_OK) == 0)
+	{
+		if (execve(command[0], command, env) == -1)
+			return (ft_free_p2(command), perror("execve"), -1);
+	}
+	else
+	{
+		cmd = get_path(command[0], env);
+		if (execve(cmd, command, env) == -1)
+			return (free(cmd), ft_free_p2(command), perror("execve"), -1);
+		free(cmd);
+	}
+	return (0);
+}
+*/
--- pipex_main_bonus.c
+++ pipex_main_bonus.c
@@ -1,20 +1,73 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pipex_main_bonus.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: razamora <razamora@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/13 13:09:55 by razamora          #+#    #+#             */
-/*   Updated: 2024/07/21 15:17:16 by razamora         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+
+
+ int	redirect_and_exec(int m_fd[2], char *line, char **env)
+{
+	int		p_fd[2];
+	pid_t	pid;
+
+	if (pipe(p_fd) == -1)
+		return (perror("Error al crear el pipe"), 1);
+	pid = fork();
+	if (pid == -1)
+		(perror("Error al crear el hilo"), exit(1));
+	else if (pid == 0)
+	{
+		dup2(m_fd[READ], STDIN_FILENO);
+		dup2(p_fd[WRITE], STDOUT_FILENO);
+
+		close(p_fd[WRITE]);
+		close(p_fd[READ]);
+		close(m_fd[WRITE]);
+		close(m_fd[READ]);
+		ft_check_command(line, env);
+	}
+	close(m_fd[READ]);
+	close(p_fd[WRITE]);
+	m_fd[READ] = p_fd[READ];
+	return (pid);
+}
+
+void	finaly_process(int *m_fd, char **av, int argc, char **env)
+{
+	int		id;
+	int		fd_out;
+	int		status;
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		(perror("Error al crear el hilo"), exit(1));
+	else if (pid == 0)
+	{
+		fd_out = open(av[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		dup2(m_fd[READ], STDIN_FILENO);
+		dup2(fd_out, STDOUT_FILENO);
+		close(fd_out);
+		close(m_fd[READ]);
+		close(m_fd[WRITE]);
+		ft_check_command(av[argc - 2], env);
+	}
+	close(m_fd[READ]);
+	close(m_fd[WRITE]);
+	
+	id = 2;
+	while (id < argc - 1)
+	{
+		waitpid(-1, NULL, 0);
+		id++;
+	}
+}
+*/
+
 
 #include "libft/libft.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#define READ    0    /* index pipe extremo escritura */
+#define WRITE   1 
 
 char	**ft_find_path(char **envp)
 {
@@ -31,7 +84,26 @@ char	**ft_find_path(char **envp)
 	}
 	var_env = ft_split(value_path,':');	
 	return(var_env);
-	
+}
+
+int open_file(char *file, int type)
+{
+	int ret;
+	if (type == 0)
+		ret = open(file, O_RDONLY, 0644);
+	if (type == 1)
+		ret = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (type == 2)
+		ret = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	if (ret == -1)
+		(perror(file));
+	return (ret);
+}
+
+void	ft_error(char *str, int code)
+{
+	ft_putendl_fd(str, 2);
+	exit(code);
 }
 
 void exec_command(char *cmd, char *cmd_route, char *envp, char **full_cmd)
@@ -50,17 +122,35 @@ void exec_command(char *cmd, char *cmd_route, char *envp, char **full_cmd)
 //	printf("%d", flag);
 //	exit(0);
 }
-
-void ft_check_command(char **path, char *cmd, char **envp)
+void ft_display(char *cmd, char *route)
+{
+	char *file = "comandos.log";
+	 FILE *fp = fopen(file, "w"); // Open file in write mode
+	if (fp == NULL) {
+		printf("Error: Cannot open file for writing!\n");
+		return;
+	}
+	fprintf(fp, " | comando: %s | ", cmd);
+	fprintf(fp, " | route: %s | ", route);
+	fprintf(fp, "\n");
+	fclose(fp);
+}
+void	ft_check_command(char *cmd, char **envp)
 {
 	int		i;
 	char	**full_cmd;
+	char	**path;
 
 	i = 0;
+	
 	full_cmd = ft_split(cmd, ' ');
 	cmd = ft_strjoin("/", full_cmd[0]);
 	//exec_command(cmd, path[i], *envp,full_cmd);
+//	ft_display(cmd, path[i]);
 	//printf("exec command  %s %s \n", cmd, path[i]);
+	path = ft_find_path(envp);
+	// if (path == NULL)
+	// 	ft_error("Error: PATH not found", 2);
 	while (path[i] != NULL)
 	{
 		path[i] = ft_strjoin(path[i], cmd);
@@ -68,86 +158,223 @@ void ft_check_command(char **path, char *cmd, char **envp)
 			exec_command(cmd, path[i], *envp,full_cmd);
 		i++;
 	}
-	printf("command not found");
-	exit(1);
+	ft_putstr_fd("command not found---->", 2);
+	ft_putstr_fd(cmd, 2);
+	exit(0);
 }
 // child
 // fd = open(O_CREAT | O_RDWR | O_TRUNC, 0644)
-void	process_one(char **argv, char **envp)
-{
-	int	fd;
-	char **path;
+// void	process_one(char **argv, char **envp, int *file_pipe)
+// {
+// 	int		pid;
+// 	char	**path;
+// 	int		fd;
 
-	fd = open(argv[1], O_RDONLY | O_CREAT, 777);
-	if (fd < 0)
-		exit(1);
-	dup2(fd, STDIN_FILENO);
-	//dup2(file_pipe[1],1);
-	//close(file_pipe[0]);
-	//path = ft_find_path(envp);
-	//ft_check_command(path, argv[2], envp);
-	close(fd);
+// 	pid = fork();
+// 	if (pid == -1)
+// 		(perror("Error en fork"), exit(1));
+// 	if (pid == 0)
+// 	{
+// 		close(file_pipe[0]);
+// 		fd = open(argv[1], 0);
+// 		dup2(fd, 0);
+// 		dup2(file_pipe[1], 1);
+// 		close(file_pipe[0]);
+// 		close(fd);
+// 		close(file_pipe[1]);
+// 		ft_check_command(argv[2], envp);
+// 	}
+// 	close(file_pipe[1]);
+// }
+
+// void	process_one(char **argv, char **envp, int *file_pipe)
+// {
+// 	int		pid;
+// 	char	**path;
+// 	int		fd;
+
+// 	pid = fork();
+// 	if (pid == -1)
+// 		(perror("Error en fork"), exit(1));
+// 	if (pid == 0)
+// 	{
+// 		close(file_pipe[0]);
+// 		fd = open(argv[1], 0);
+// 		dup2(fd, 0);
+// 		dup2(file_pipe[1], 1);
+// 		close(file_pipe[0]);
+// 		close(fd);
+// 		close(file_pipe[1]);
+// 		ft_check_command(argv[2], envp);
+// 	}
+// 	close(file_pipe[1]);
+// }
+
+
+
+
+
+void	process_one(char **argv, char **envp, int *file_pipe)
+{
+	int		pid;
+	int		fd;
+
+	pid = fork();
+	if (pid == -1)
+		(perror("Error en fork"), exit(0));
+	if (pid == 0)
+	{
+		close(file_pipe[0]);
+		fd = open(argv[1], O_RDONLY, 0644);
+		dup2(fd, 0);
+		dup2(file_pipe[1], 1);
+		close(file_pipe[0]);
+		close(fd);
+		close(file_pipe[1]);
+		ft_check_command(argv[2], envp);
+	}
+	close(file_pipe[1]);
 }
 
-pid_t	middle_process(char *cmd ,char **envp, int i)
+// int	middle_process(char *cmd ,char **envp, int m_fd[2])
+// {
+// 	pid_t	pid;
+// 	int		p_fd[2];
+
+	
+// 	if (pipe(p_fd) == -1)
+// 		return (perror("Error al crear el pipe"), 1);
+// 	pid = fork();
+// 	if (pid == -1)
+// 		(perror("Error al crear el hilo"), exit(1));
+// 	else if (pid == 0)
+// 	{
+// 		dup2(m_fd[READ], STDIN_FILENO);
+// 		dup2(p_fd[WRITE], STDOUT_FILENO);
+
+// 		close(p_fd[WRITE]);
+// 		close(p_fd[READ]);
+// 		close(m_fd[WRITE]);
+// 		close(m_fd[READ]);
+// 		ft_check_command(cmd, envp);
+// 	}
+// 	close(m_fd[READ]);
+// 	close(p_fd[WRITE]);
+// 	m_fd[READ] = p_fd[READ];
+// 	return (pid);
+// }
+
+int	middle_process(char *cmd ,char **envp, int file_pipe[2])
 {
-	pid_t id;
-	int file_pipe[2];
-	int status;
-	char **path;
-	if (pipe(file_pipe) == -1)
+	pid_t	id;
+	int		middle_file[2];
+
+	if (pipe(middle_file) == -1)
 		perror ("Error:");
 	id = fork();
-
 	if (id == -1)
 		perror ("Error:");
-	if (id == 0)
+	else if (id == 0)
 	{
-		close(file_pipe[0]);
-		dup2(file_pipe[1], STDOUT_FILENO);
-		close(file_pipe[1]);
-		path = ft_find_path(envp);
-		ft_check_command(path, cmd, envp);
-		
-	}
-	else
-	{
-		close(file_pipe[1]);
 		dup2(file_pipe[0], STDIN_FILENO);
+		dup2(middle_file[1], STDOUT_FILENO);
+
+		close(file_pipe[1]);
 		close(file_pipe[0]);
+		close(middle_file[1]);
+		close(middle_file[0]);
+		ft_check_command(cmd, envp);
 	}
+	close(file_pipe[0]);
+	close(middle_file[1]);
+	file_pipe[0] = middle_file[0];
 	return (id);
 }
 
-void	process_two(char **argv, char **envp, int argc, pid_t *pids)
+int	redirect_and_exec(int m_fd[2], char *line, char **env)
 {
-	char	**path;
+	int		p_fd[2];
 	pid_t	pid;
-	int		fd;
-	int		i;
-	int		status;
 
-	fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipe(p_fd) == -1)
+		return (perror("Error al crear el pipe"), 1);
 	pid = fork();
 	if (pid == -1)
 		(perror("Error al crear el hilo"), exit(1));
 	else if (pid == 0)
 	{
-		dup2(fd, STDOUT_FILENO);
-		path = ft_find_path(envp);
-	//	close(fd);
-		ft_check_command(path, argv[argc - 2], envp);
+		dup2(m_fd[READ], STDIN_FILENO);
+		dup2(p_fd[WRITE], STDOUT_FILENO);
+
+		close(p_fd[WRITE]);
+		close(p_fd[READ]);
+		close(m_fd[WRITE]);
+		close(m_fd[READ]);
+		ft_check_command(line, env);
 	}
-	
-	pids[argc - 2] = pid;
-	i = 1;
-	status = 0;
+	close(m_fd[READ]);
+	close(p_fd[WRITE]);
+	m_fd[READ] = p_fd[READ];
+	return (pid);
+}
+
+void	process_two(char **argv, char **envp, int argc, int *file_pipe)
+{
+	pid_t	pid;
+	int		fd;
+	int		i;
+	pid = fork();
+	if (pid == -1)
+		(perror("Error al crear el fork"), exit(1));
+	if (pid == 0)
+	{
+		fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		dup2(file_pipe[0], 0);
+		dup2(fd,1);
+		close(fd);
+		close(file_pipe[0]);
+		close(file_pipe[1]);
+		ft_check_command(argv[argc - 2], envp);
+	}
+	close(file_pipe[0]);
+	close(file_pipe[1]);
+	i = 2;
+	// esperamos todos los subprocesos que no jan terminado
 	while (i < argc - 1)
 	{
-		waitpid(pids[i], &status, 0);
+		waitpid(-1, NULL, 0);
 		i++;
 	}
+}
+void	finaly_process(int *m_fd, char **av, int argc, char **env)
+{
+	int		id;
+	int		fd_out;
+	int		status;
+	pid_t	pid;
 
+	pid = fork();
+	if (pid == -1)
+		(perror("Error al crear el hilo"), exit(1));
+	else if (pid == 0)
+	{
+		fd_out = open(av[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		dup2(m_fd[READ], STDIN_FILENO);
+		dup2(fd_out, STDOUT_FILENO);
+		close(fd_out);
+		close(m_fd[READ]);
+		close(m_fd[WRITE]);
+		ft_check_command(av[argc - 2], env);
+	}
+	close(m_fd[READ]);
+	close(m_fd[WRITE]);
+	
+	id = 2;
+	while (id < argc - 1)
+	{
+		waitpid(-1, NULL, 0);
+		id++;
+	}
 }
 
 pid_t	ft_here_doc(char *str)
@@ -182,118 +409,36 @@ int	main(int argc, char **argv, char **envp)
 {
 	int	*id;
 	int	i;
-	int	status;
-	pid_t	pids[argc - 1];
-
-	status = 0;
-	if (argc < 5)
-		return (-1);
-	i = 2;
-	if (ft_strncmp("here_doc",argv[1],8) == 0)
-	{
-		if (argc < 6)
-			perror("falta argc");
-		id[i++ - 2] = ft_here_doc(argv[2]);
-	}
-	else
-		process_one(argv, envp);
-	while (i < argc - 2)
-	{
-		pids[i - 2] = middle_process(argv[i] ,envp, i);
-		i++;
-	}
-	process_two(argv, envp ,argc, pids);
-	return (0);
-}
-
-
-
-/*
-
-void    finaly_process(char **av, int argc, char **env, pid_t *pids)
-{
-	pid_t    pid;
-	int        fd_out;
-	int        i;
-	int        status;
-
-	fd_out = open_file(av[argc - 1], STDOUT_FILENO);
-	pid = fork();
-	if (pid == -1)
-		(perror("Error al crear el hilo"), exit(1));
-	else if (pid == 0)
-	{
-		dup2(fd_out, 1);
-		exec_cmd(av[argc - 2], env);
-		close(fd_out);
-	}
-	pids[argc - 2] = pid;
-	i = 1;
-	status = 0;
-	while (i < argc - 1)
-	{
-		waitpid(pids[i], &status, 0);
-		i++;
-	}
-}
-*/
-/*
-int	main(int argc, char **av, char **env)
-{
-	int		i;
-	int		fd_in;
-	int		fd_out;
+	int file_pipe[2];
 
 	if (argc < 5)
-		return (-1);
-	i = 2;
-	if (ft_strcmp(av[1], "here_doc") == 0)
-	{
-		if (argc < 6)
-			(perror("d"), exit(0));
-		i = 3;
-		fd_out = open_file(av[argc - 1], 2);
-		here_doc(av[2]);
-	}
-	else
-	{
-		fd_in = open_file(av[1], STDIN_FILENO);
-		fd_out = open_file(av[argc - 1], STDOUT_FILENO);
-		dup2(fd_in, STDIN_FILENO);
-	}
-	while (i < (argc - 2))
-		redirect_and_exec(av[i++], env);
-	dup2(fd_out, STDOUT_FILENO);
-	(exec_cmd(av[argc - 2], env), close(fd_out));
-}
-*/
-
-
-/*
-static int here_doc(char *limiter)
-{
-	int		p_fd[2];
-	pid_t	pid;
-	int	status;
-	char *p_heredoc;
+		(perror("minimum 5 argc"), exit(1));
+	if (pipe(file_pipe) == -1)
+		exit(0);
+	// process_one(argv, envp, file_pipe);
+	// i = 3;
+	// if (ft_strncmp("here_doc",argv[1],8) == 0)
+	// {
+	// 	if (argc < 6)
+	// 		perror("falta argc");
+	// 	ft_here_doc(argv[2]);
+	// }
+	// else
 	
-	if (pipe(p_fd) == -1)
-		return (perror("Error al crear el pipe"), 1);
-	pid = fork();
-	if (pid == -1)
-		(perror("Error al crear el hilo"), exit(1));
-	else if (pid == 0)
-	{
-		close(p_fd[0]);
-		while(1)
-		{
-			p_heredoc = get_next_line(0);
-			if (ft_strncmp(p_heredoc, limiter, ft_strlen(limiter)) == 0)
-				(free(p_heredoc), exit(0));
-			(ft_putstr_fd(p_heredoc, p_fd[1]), free(p_heredoc));
-		}
-	}
-	else
-		(waitpid(pid, &status, 0), close(p_fd[1]), dup2(p_fd[0], 0));
+	i = 2;
+	//first_process(argv, file_pipe, envp);
+	process_one(argv, envp, file_pipe);
+	i++;
+	while (i < (argc - 2))
+		//i += ((redirect_and_exec(file_pipe, argv[i], envp)), 1);
+		//(redirect_and_exec(file_pipe, argv[i++], envp));
+		middle_process(argv[i++] ,envp,file_pipe);
+	//finaly_process(file_pipe, argv, argc, envp);
+	process_two(argv, envp, argc,file_pipe);
+	// while (i < argc - 2)
+	// {
+	// 	i += middle_process(argv[i] ,envp, file_pipe);
+	// }
+	// process_two(argv, envp ,argc, file_pipe);
+	//return (0);
 }
-*/
